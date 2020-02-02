948433ccb68ba7397005a6efb86054731b54bbcc
Breno Moura <basmoura@gmail.com>
2020-02-02 20:04:11 -0300

    add controller and request specs for admin

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,19 @@
+class AdminsController < ApplicationController
+  def create
+    @admin = Admin.new(admin_params)
+
+    if @admin.save
+      render json: @admin, status: :created
+    else
+      render json: { errors: @admin.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def admin_params
+    params.require(:admin).permit(:name, :email, :password,
+                                  :password_confirmation)
+  end
+end
---
+++ spec/requests/admins_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Admins', type: :request do
+  describe 'POST admin' do
+    context 'when has valid attributes' do
+      let(:admin_attributes) { FactoryBot.attributes_for(:admin) }
+
+      it 'creates a new Admin' do
+        expect do
+          post admins_path, params: { admin: admin_attributes }
+        end.to change(Admin, :count).by(1)
+      end
+    end
+
+    context 'when has invalid attributes' do
+      let(:admin_invalid_attributes) { FactoryBot.attributes_for(:admin, email: nil) }
+
+      it 'returns 422' do
+        post admins_path, params: { admin: admin_invalid_attributes }
+
+        expect(response.status).to eq 422
+      end
+    end
+  end
+end
