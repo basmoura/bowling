5fe44b3e8eb4d8af4f4744929e3eef6c4e9cf632
Breno Moura <basmoura@gmail.com>
2020-02-03 10:54:59 -0300

    Add missing specs for admin model

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe Admin, type: :model do
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_length_of(:password).is_at_least(6) }
+  it { is_expected.to have_many(:bowling_houses) }
 end
