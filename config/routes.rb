0545f52c0c063002bf881ab5ea8cf96daade4954
Breno Moura <basmoura@gmail.com>
2020-02-03 15:04:29 -0300

    add routes for lanes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   post '/auth/login', to: 'auth#login'
+
   resources :admins, only: [:create]
-  resources :bowling_houses, only: [:index, :create]
+  resources :bowling_houses, only: [:index, :create] do
+    resources :lanes, only: [:index, :create, :update]
+  end
 end
