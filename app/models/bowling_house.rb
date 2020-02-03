d36bf1df9191206c97df264b9ab400a03fa60ff8
Breno Moura <basmoura@gmail.com>
2020-02-03 15:04:43 -0300

    add has_many association with lanes

--- app/models/bowling_house.rb
+++ app/models/bowling_house.rb
@@ -1,4 +1,8 @@
 class BowlingHouse < ApplicationRecord
+  has_many :lanes, dependent: :destroy
+  accepts_nested_attributes_for :lanes,
+                                allow_destroy: true,
+                                reject_if: :all_blank
   belongs_to :admin
   validates :name, presence: true
 end
